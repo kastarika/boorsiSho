b2d5482dfbcdc8c0ea802ef981202a334f4209b0
kasra <kakaee.kasra@gmail.com>
2019-07-28 09:03:52 +0430

    added readfromdatabase

--- boorsiSho/mainPage/urls.py
+++ boorsiSho/mainPage/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('page2',views.search)
 ]
--- boorsiSho/mainPage/views.py
+++ boorsiSho/mainPage/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import readFromDatabase
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello")
+def fillInDatabase( request ):
+    readFromDatabase.fillInDatabase()
+    return HttpResponse("DONE")
+    
